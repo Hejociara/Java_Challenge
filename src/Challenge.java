d65a763d095ff144190cb101cbf1ab75b375fb75
Hejociara <hejociara@gmail.com>
2022-08-27 20:38:30 -0300

    "Feature"

--- src/Challenge.java
+++ src/Challenge.java
@@ -4,6 +4,15 @@ public class Challenge {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter your first salary: ");
+        input.nextLine();
+        System.out.println("Enter your second salary: ");
+        input.nextLine();
+        System.out.println("Enter your last salary: ");
+        input.nextLine();
+        System.out.println();
+
+
         input.close();
     }
 }
